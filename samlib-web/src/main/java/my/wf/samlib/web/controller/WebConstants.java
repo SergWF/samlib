8b3309552d95c115764c74798c8d8184df1dc488
Sergii Bilenogov <sbilenogov@affinitas398.corp.affinitas.de>
2015-06-02 15:02:13 +0200

    Remove unnecessary stuff

--- samlib-web/src/main/java/my/wf/samlib/web/controller/WebConstants.java
+++ samlib-web/src/main/java/my/wf/samlib/web/controller/WebConstants.java
@@ -2,9 +2,6 @@ package my.wf.samlib.web.controller;
 
 public class WebConstants {
     public static final String HOME_VIEW = "home";
-    public static final String LOGIN_VIEW = "login";
-    public static final String HELLO_VIEW = "hello";
-
     public static final String AUTHORS_LIST = "fragments :: authorList";
     public static final String AUTHOR_DETAILS = "fragments :: authorDetails";
 }
